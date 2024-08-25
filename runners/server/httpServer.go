842d02494c9b730fbc0ef23483ddf9f53d6f8752
arubek <4040gold.dev@gmail.com>
2024-08-25 16:51:41 +0900

    httpServerのStart()関数を追加

--- runners/server/httpServer.go
+++ runners/server/httpServer.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -45,5 +46,9 @@ func InitHttpServer(config *viper.Viper, dbHandler *sql.DB) HttpServer {
 }
 
 func (hs HttpServer) Start() {
-
+	err := hs.router.Run(hs.config.GetString(
+		"http.server_address"))
+	if err != nil {
+		log.Fatalf("Error while starting HTTP server: %v", err)
+	}
 }
