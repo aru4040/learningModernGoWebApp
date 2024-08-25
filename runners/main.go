4cc83d2c7e689c1bea5ede06c8be6802cd0135a3
arubek <4040gold.dev@gmail.com>
2024-08-25 15:09:50 +0900

    main.goを実装

--- runners/main.go
+++ runners/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	log.Println("Starting Runners App")
+
+	log.Println("Initializing configuration")
+	config := config.InitConfig("runners")
+
+	log.Println("Initializing database")
+	dbHandler := server.InitDatabase(config)
+
+	log.Println("Initializing HTTP server")
+	httpServer := server.InitHttpServer(config, dbHandler)
+	httpServer.Start()
 }
