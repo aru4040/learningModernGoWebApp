f8d732dab727911a92e4fc76f7d33a4f299350fa
arubek <4040gold.dev@gmail.com>
2024-08-25 16:48:34 +0900

    InitHttpServer関数を追加

--- runners/server/httpServer.go
+++ runners/server/httpServer.go
@@ -11,10 +11,36 @@ type HttpServer struct {
 	config            *viper.Viper
 	router            *gin.Engine
 	runnersController *controllers.RunnersController
-	resultController  *controllers.ResultController
+	resultsController *controllers.ResultController
 }
 
 func InitHttpServer(config *viper.Viper, dbHandler *sql.DB) HttpServer {
+	runnersRepository := repositories.NewRunnersRepository(dbHandler)
+	resultsRepository := repositories.NewResultsRepository(dbHandler)
+
+	runnersService := services.NewRunnersService(runnersRepository, resultsRepository)
+	resultsService := services.NewResultsService(resultsRepository, runnersRepository)
+
+	runnersController := controllers.NewRunnersController(runnersService)
+	resultsController := controllers.NewResultsController(resultsService)
+
+	router := gin.Default()
+
+	router.POST("/runner", runnersController.CreateRunner)
+	router.PUT("/runner", runnersController.UpdateRunner)
+	router.DELETE("/runner/:id", runnersController.DeleteRunner)
+	router.GET("/runner/:id", runnersController.GetRunner)
+	router.GET("/runner", runnersController.GetRunnerBatch)
+
+	router.POST("/result", resultsController.CreateResult)
+	router.DELETE("/result/:id", resultsController.DeleteResult)
+
+	return HttpServer{
+		config:            config,
+		router:            router,
+		runnersController: runnersController,
+		resultsController: resultsController,
+	}
 
 }
 
